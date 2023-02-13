7be1641ef1ab4ad3f8d6c0dd292d891e2fc79543
Zahid <muhammadzahid11@gmail.com>
2023-02-13 16:03:57 +0100

    Code Sekelton is written.

--- magzine.py
+++ magzine.py
@@ -8,6 +8,7 @@ import argparse
 import logging
 import os
 import sys
+from typing import list, tuple
 
 
 __VERSION__ = "0.1"
@@ -64,6 +65,48 @@ def get_args():
             "{}".format(excep1)
         )
         sys.exit(1)
+
+
+def is_article_valid(article: str) -> bool:
+    """Check article format is valid or not. Return True for valid article
+    otherwise false.
+
+    e.g: 10-15 is article containing  pages from 10 to 15. Check whether
+    hyphen '-' exist between ranges then check page range contains only
+    integers.
+    """
+    pass
+
+
+def check_overlapping_articles(article_1: str, article_2: str) -> str:
+    """Check which pages of 2 articles are overlapping and return those pages.
+
+    article_1: First article contains the page range from magzine. e.g: 10-15
+    article_2: 2nd article contains the page range from magzine. e.g: 12-15
+    return overlapping articles i.e 12-15
+    """
+    pass
+
+
+def seperate_valid_invalid_articles_from_line(
+        file_line: str) -> tuple[str, str]:
+    """Seperate valid and invalid articles from each line.
+
+    file_line: it is a line string
+    Returns the tuple containing two string elements.
+    Two articles are seperated by comma ';'
+    i.e ("<valid_article_1, valid_article_2>",
+    <invalid_article_1, invalid_article_2>)
+    """
+    pass
+
+
+def read_articles_from_file(file_name: str) -> list[tuple[str, str]]:
+    """Read articles from file line by line and seperate valid and invalid
+    articles into two files w.r.t to each line.
+
+    """
+    pass
 
 
 def main():
